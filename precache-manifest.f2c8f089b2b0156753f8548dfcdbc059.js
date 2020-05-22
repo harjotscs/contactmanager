604b1a457f6767ca57be89530f30d6599bcd1bd0
harjotscs <harjotscs@noreply.github.com>
2020-05-22 07:05:16 +0530

    Updates

---
+++ precache-manifest.f2c8f089b2b0156753f8548dfcdbc059.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683ef2ce09ccf2204ac89faaf5b90f24",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "c2c1e93c6ede181aa1a4",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ed4a9a54818976bfd485",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c2c1e93c6ede181aa1a4",
+    "url": "/contactmanager/static/js/2.e83c403d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.e83c403d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed4a9a54818976bfd485",
+    "url": "/contactmanager/static/js/main.93a16216.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
